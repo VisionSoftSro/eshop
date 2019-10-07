af0a825ad04cd3a241fd5848fabd804dda842c75
tremll <lukas.treml@t-mobile.cz>
2019-10-07 17:30:39 +0200

    remove unneccesera files

---
+++ c-frontend/src/web/pages/Root.tsx
@@ -0,0 +1,29 @@
+import * as React from "react";
+import {connect} from "react-redux";
+import {WebReducersState} from "../redux/WebRedux";
+import Wrapper from "../../common/component/Wrapper";
+
+import {LocaleState} from "../../common/redux/reducers/locale/LocaleReducer";
+import {changeLocale} from "../redux/reducers/LocaleActions";
+import DataStorage from "../../common/DataStorage";
+import moment from "moment";
+
+
+class Root extends React.Component<LocaleState> {
+
+
+    componentDidMount(): void {
+        this.changeLocale(DataStorage.get("locale")||"cs");
+    }
+
+    changeLocale = (locale:string) => {
+        this.props.dispatch(changeLocale(locale));
+    };
+
+    render() {
+        return <Wrapper>
+            eshop
+        </Wrapper>;
+    }
+}
+export default connect((state:WebReducersState) => state.locale)(Root);
