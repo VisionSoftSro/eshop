b1b181cbd05eaab1b8b8e5ac7301b0f9f0bde2fe
Lukas Treml <lukas.treml@t-mobile.cz>
2019-11-26 14:08:18 +0100

    fixed configuration + ddl

--- c-domain/src/main/resources/db/migration/V1.0.0__create_initial_ddl.sql
+++ c-domain/src/main/resources/db/migration/V1.0.0__create_initial_ddl.sql
@@ -2,8 +2,8 @@ create table goods (
     id text not null,
     name text not null,
     description text not null,
-    stock text not null,
-    price text not null,
+    stock integer not null,
+    price decimal not null,
     hot boolean not null default false,
     images integer default 0
 );
