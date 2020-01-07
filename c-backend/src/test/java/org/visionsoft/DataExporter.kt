4fd70ca2d800764d5e2fe924bc2bdd1bedc3d65b
Lukas Treml <lukas.treml@t-mobile.cz>
2020-01-07 18:18:08 +0100

    working on mechanism which parse source data from google drive to sql

---
+++ c-backend/src/test/java/org/visionsoft/DataExporter.kt
@@ -0,0 +1,110 @@
+package org.visionsoft
+
+import com.sun.org.apache.xerces.internal.util.DOMUtil.getParent
+import com.sun.tools.corba.se.idl.Util.getAbsolutePath
+import org.apache.commons.io.FilenameUtils
+import org.springframework.util.StringUtils
+import java.io.*
+import java.io.File.separator
+import java.util.zip.ZipEntry
+import java.util.zip.ZipInputStream
+import java.math.BigDecimal
+import java.text.MessageFormat
+import javax.xml.crypto.Data
+
+val mapping = mapOf("gift" to "Dárky", "parties" to "Oslavy")
+val exportPath = "/Users/tremll/devel/eshop-export/"
+fun main() {
+    mapping.forEach {
+        DataExporter("/Users/tremll/Google Drive/${it.value}/", it.key).export()
+    }
+}
+
+class DataExporter(val path: String, val category: String) {
+    val sqlBuffer = StringBuffer()
+    var index = 0
+    fun export() {
+        val root = File(path)
+        if (root.isDirectory) {
+            root.list()?.forEach { d ->
+                val productFolder = File("$path$d")
+                if (productFolder.isDirectory) {
+                    val process = ProductProcessor(index++, category, productFolder)
+                    process.process()
+                    sqlBuffer.append(process.sqlBuffer)
+                }
+            }
+        }
+        save()
+    }
+
+    fun save() {
+        println(sqlBuffer)
+    }
+}
+
+data class SqlTemplateData(val code: String, val name: String, val description: String, val hot: Boolean, val images: Int, val stock: Int, val price: BigDecimal) {
+    fun toSql() = "insert into goods (code, name, description, hot, images, stock, price) values ('${code}', '${name}', '${description}', ${hot}, ${images}, ${stock}, ${price});"
+}
+class ProductProcessor(val index: Int, val category: String, val productFolder: File) {
+    val sqlBuffer = StringBuffer()
+    val images:Int = 0
+    fun process() {
+        processImages()
+        processSql()
+    }
+
+    fun processSql() {
+
+        productFolder.list()?.firstOrNull { it == "Text.txt" }?.let {
+            val file = File("${productFolder.absolutePath}/${it}")
+            if(it == "Text.txt") {
+                InputStreamReader(FileInputStream(file), "ISO-8859-2").use { txt->
+                    parseData(txt.readText())?.let {tmpl->
+                        sqlBuffer.append(tmpl.toSql())
+                        sqlBuffer.append("\n")
+                        sqlBuffer.append("insert into goods_category(category, goods) values ('${category}', currval('goods_id_seq'));")
+                        sqlBuffer.append("\n")
+                    }
+                }
+            }
+        }
+
+    }
+
+    fun parseData(text:String):SqlTemplateData? {
+        val rows = text.split("\n")
+        val firstRow = rows[0]
+        return parsePriceAndStock(firstRow)?.let {
+            val descriptionRows = rows.subList(2, rows.size)
+            val name = productFolder.name
+            SqlTemplateData("${category}-${index}", name, formatDescription(descriptionRows), false, images, it.first, it.second)
+        }
+    }
+
+    fun formatDescription(rows:List<String>):String {
+        return rows.map { it.replace("\r", "") }.joinToString("") { if (it == "") {"<br/><br/>"} else {"${it}<br/>"} }
+    }
+
+    fun parsePriceAndStock(row:String):Pair<Int, BigDecimal>? {
+        if(!StringUtils.isEmpty(row)) {
+            println("${productFolder} -> ")
+            val cols = row.split("-")
+            var startIndex = 1
+            if(cols.size == 6) {
+                startIndex = 2
+            }
+            val priceCol = cols[startIndex]
+            val price = priceCol.trim().let { it.substring(0, it.indexOf("k")).trim().toBigDecimal() }
+            val stockCol = cols[startIndex+3]
+            val stock = stockCol.trim().let { it.substring(0, it.indexOf("k")).trim().toInt() }
+            return stock to price
+        }
+
+        return null
+    }
+
+    fun processImages() {
+
+    }
+}
