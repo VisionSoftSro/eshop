a295e14abe24d75ed0a7d3059b085f821cbcecc4
herisn23 <herisn23@gmail.com>
2020-02-26 18:16:54 +0100

    improve seo
    Took 17 minutes

--- c-frontend/src/index.js
+++ c-frontend/src/index.js
@@ -3,8 +3,8 @@ import "babel-polyfill";
 import 'proxy-polyfill';
 
 import * as serviceWorker from './serviceWorker';
+document.getElementById("fake-seo").remove();
 import './web/Initialization';
-
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
 // Learn more about service workers: https://bit.ly/CRA-PWA
